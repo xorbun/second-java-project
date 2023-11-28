794db1d7c0c6e778990203c2d91c8911c4993d2c
Bruno Capobianco <bruno.capobianco@outlook.it>
2023-11-28 17:26:44 +0100

    xorbun
    some bug fixed

--- src/esercizio1/main2.java
+++ src/esercizio1/main2.java
@@ -4,11 +4,7 @@ public class main2
 {
     public static boolean happynewyear(int anno)
     {
-        if (anno % 4 == 0)
-        {
-            return true;
-        }
-        else if (anno % 100 == 0 && anno % 400 == 0)
+        if (anno % 4 == 0 && anno % 100 == 0 && anno % 400 == 0)
         {
             return true;
         }
